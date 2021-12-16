064fe506b2b1d722fba693d35d2a825a54427288
Marlon Lewis <marlonlewis483@gmail.com>
2021-12-16 08:01:20 -0500

    js file updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,7 @@
 window.onload = function(){
-
     $("#btn").click(function(e){
         e.preventDefault();
-        $.get("./superheroes.php?hero="+String($("#hero").val()).trim(),
-        function(data){
+        $.get("./superheroes.php?hero="+$("#hero").val().trim(),function(data){
             $('#result').html(data);
             $("#hero").val("");
         })
